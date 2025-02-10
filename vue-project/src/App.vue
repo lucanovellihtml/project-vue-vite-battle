4a81fe49bad3be1bc955d0fa8ad11e7caeeae89b
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-02-10 17:18:07 +0100

    add: imported children components

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,77 @@
-<script setup>
+<script>
+
+import { ref } from "vue"
+import HeaderGame from "./components/HeaderGame.vue";
+import Player from "./components/Player.vue";
+import ButtonControl from "./components/ButtonControl.vue";
+import LogBattle from "./components/logBattle.vue";
+
+export default {
+
+  components: { HeaderGame, Player, ButtonControl, LogBattle }
+
+}
+
 </script>
 
 <template>
+
+  <!-- header -->
+  <header>
+
+    <!-- header component -->
+    <HeaderGame></HeaderGame>
+
+  </header>
+
+  <!-- main -->
+  <main>
+
+    <!-- section enemy -->
+    <Player title="Enemy"></Player>
+
+    <!-- section player -->
+    <Player title="You"></Player>
+
+    <!-- section controls -->
+    <section class="container-fluid mt-5 d-flex flex-column gap-4">
+
+      <!-- row buttons attack -->
+      <div class="row justify-content-center gap-4 gap-sm-0">
+
+        <!-- button attack -->
+        <div class="col-12 col-sm-6 col-md-3 text-center">
+          <ButtonControl title="Attack" color="primary" />
+        </div>
+
+        <!-- button attack special -->
+        <div class="col-12 col-sm-6 col-md-3 text-center">
+          <ButtonControl title="Special Attack" color="primary" />
+        </div>
+
+      </div>
+
+      <!-- row buttons medikit/gamer over -->
+      <div class="row justify-content-center gap-4 gap-sm-0">
+
+        <!-- button meidkit -->
+        <div class="col-12 col-sm-6 col-md-3 text-center">
+          <ButtonControl title="Medikit" color="success" />
+        </div>
+
+        <!-- button gamer over -->
+        <div class="col-12 col-sm-6 col-md-3 text-center">
+          <ButtonControl title="Gamer Over!" color="danger" />
+        </div>
+
+      </div>
+
+    </section>
+
+    <!-- section log -->
+    <LogBattle title="Battle log" />
+
+  </main>
 
 </template>
 
