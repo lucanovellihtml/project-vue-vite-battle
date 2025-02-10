60c973671e96f7a3d92c30171348d735d67eaf00
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-02-10 17:34:22 +0100

    update: added property round, it's a variable counts every round

--- vue-project/src/components/HeaderGame.vue
+++ vue-project/src/components/HeaderGame.vue
@@ -5,11 +5,39 @@
             <div class="col p-4">
 
                 <!-- title -->
-                <h1 class="fs-1">Game</h1>
+                <h1 class="fs-1 fw-bold">Game | {{ round }}</h1>
 
             </div>
         </div>
     </div>
 
-
 </template>
+
+<script>
+
+export default {
+
+    //name component
+    name: "HeaderGame",
+
+    props: {
+
+        //props count round
+        round: {
+            type: String,
+            required: true,
+            default: "0"
+        }
+    },
+
+    setup(props) {
+
+
+        return {
+
+        }
+
+    }
+}
+
+</script>
